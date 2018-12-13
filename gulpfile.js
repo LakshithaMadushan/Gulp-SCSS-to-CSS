c964372cfcc8eabf06bc039b9949dde336be5153
LakshithaMadushan <LakshithaMadushan@users.noreply.github.com>
2018-12-13 12:37:17 +0530

    Configure-gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,36 @@ gulpSequence = require('gulp-sequence').use(gulp);
 shell = require('gulp-shell');
 sourceMaps = require('gulp-sourcemaps');
 plumber = require('gulp-plumber');
+
+gulp.task('default', [ 'sass', 'serve' ]);
+
+gulp.task('sass', function() {
+	gulp
+		.src('src/app/scss/**/*.scss')
+		.pipe(
+			plumber({
+				errorHandler: function(err) {
+					console.log(err);
+					this.emit('end');
+				}
+			})
+		)
+		.pipe(sourceMaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.on('error', gutil.log)
+		.pipe(concat('body.css'))
+		.pipe(sourceMaps.write())
+		.pipe(gulp.dest('src/app/css'))
+		.pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('serve', function() {
+	browserSync.init({
+		port: 3322,
+		server: 'src'
+	});
+
+	gulp.watch('src/app/scss/**/*.scss', [ 'sass' ]);
+	gulp.watch('src/*.html').on('change', browserSync.reload);
+});
