8400e4cb41c0d9467e370dad3f116a1007b0f259
LakshithaMadushan <LakshithaMadushan@users.noreply.github.com>
2018-12-13 10:47:38 +0530

    Init-SCSS

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+gulp = require('gulp');
+sassLint = require('gulp-sass-lint');
+gutil = require('gulp-util');
+concat = require('gulp-concat');
+uglify = require('gulp-uglify');
+sass = require('gulp-sass');
+sourceMaps = require('gulp-sourcemaps');
+imagemin = require('gulp-imagemin');
+minifyCSS = require('gulp-minify-css');
+browserSync = require('browser-sync').create();
+autoprefixer = require('gulp-autoprefixer');
+gulpSequence = require('gulp-sequence').use(gulp);
+shell = require('gulp-shell');
+sourceMaps = require('gulp-sourcemaps');
+plumber = require('gulp-plumber');
